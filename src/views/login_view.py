70abbf1ff7d8ce7e6463701c97f8bef1399f8615
Yohan <yohanm3@gmail.com>
2026-04-01 13:54:00 -0400

    feat: maquetación de LoginView con márgenes internos y máscara de clave

---
+++ src/views/login_view.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import messagebox
+
+from src.utils.settings import COLOR_FONDO, COLOR_TEXTO, FUENTE_TITULO, FUENTE_NORMAL, COLOR_PRIMARIO
+class LoginView:
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Cerpint Mayor - Acceso")
+        self.root.geometry("400x500")
+        self.root.configure(bg=COLOR_FONDO)
+        self.root.resizable(False,False)
+
+        self.crear_widgets()
+
+    def crear_widgets(self):
+        self.label_titulo = tk.Label(
+            self.root,
+            text="CERPINT MAYOR",
+            font=FUENTE_TITULO,
+            bg=COLOR_FONDO,
+            fg=COLOR_TEXTO,
+        )
+        self.label_titulo.pack(pady=20)
+
+        # INPUT DE USUARIO Y SU LABEL
+
+        self.label_user=tk.Label(
+            self.root,
+            text="Usuario",
+            font=FUENTE_NORMAL,
+            fg=COLOR_TEXTO
+        )
+        self.label_user.pack(pady=5)
+        self.entry_cedula=tk.Entry(
+            self.root,
+            font=FUENTE_NORMAL,
+            justify="left",
+            fg=COLOR_TEXTO,
+            bg="white",
+            bd=8,
+            relief="flat",
+            highlightthickness=1,            
+        )
+        self.entry_cedula.config(highlightbackground="#CCCCCC", highlightcolor="#28a745")
+        self.entry_cedula.pack(pady=10, padx=50, fill="x", ipady=5)
+
+        # INPUT DE PASSWORD Y SU LABEL
+
+        self.label_password = tk.Label(
+            self.root, text="Clave de acceso",
+            font=FUENTE_NORMAL,
+            fg=COLOR_TEXTO
+        )
+        self.label_password.pack(pady=5)
+        self.entry_password = tk.Entry(
+            self.root,
+            font=FUENTE_NORMAL,
+            justify="left",
+            fg=COLOR_TEXTO,
+            bg="white",
+            show="●",
+            bd=8,
+            relief="flat",
+            highlightthickness=1,
+        )
+        self.entry_password.config(highlightbackground="#CCCCCC", highlightcolor="#28a745")
+        self.entry_password.pack(pady=10, padx=50, fill="x", ipady=5)
+
+        self.entry_cedula.focus_set()
+
+        # label de olvido contraseña
+        self.label_forget_password = tk.Label(
+            self.root,
+            text="¿Olvidaste tu contraseña?",
+            font=FUENTE_NORMAL,
+            fg=COLOR_PRIMARIO,
+        )
+        self.label_forget_password.pack(pady=5)
+
+if __name__=="__main__":
+  app = LoginView()
+  app.root.mainloop()
